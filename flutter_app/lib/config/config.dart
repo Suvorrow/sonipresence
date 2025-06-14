168d616d25a33161b8926c2f85e8cb83902d16ae
Suvorrow <suvoraum@gmail.com>
2025-06-14 18:43:12 +0200

    Fix the import issues in widget_test.dart file.

--- flutter_app/lib/config/config.dart
+++ flutter_app/lib/config/config.dart
@@ -1,2 +1,3 @@
-export 'screen_util_config.dart';
+export 'package:sonipresence/config/screen_util_config.dart';
+export 'package:flutter_riverpod/flutter_riverpod.dart';
 export 'app_theme.dart';
--- flutter_app/test/widget_test.dart
+++ flutter_app/test/widget_test.dart
@@ -7,13 +7,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:sonipresence/main.dart';
+import 'package:sonipresence/app/app_widget.dart';
+import 'package:sonipresence/app/routes.dart';
+import 'package:sonipresence/config/config.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+        ProviderScope(
+            child: SonipresenceApp(
+                screenUtilConfig: screenUtilConfig,
+                router: router,
+            ),
+        ),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
